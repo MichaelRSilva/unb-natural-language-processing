41ba7a30c29f8a0cf01e734f39df36d42fa92839
Michael Silva <mksilva@amazon.com>
2024-10-29 10:45:32 -0300

    add comments on code and run with 64 merges

--- exercise-01/bpe_tokenizer.py
+++ exercise-01/bpe_tokenizer.py
@@ -12,14 +12,24 @@ class BpeTokenizer:
         """Train the vocabulary using the Byte Pair Encoding (BPE) algorithm."""
         assert vocab_size >= 256
         num_merges = vocab_size - 256
+
+        # input text preprocessing
         text_bytes = text.encode("utf-8")
         ids = list(text_bytes)
+
+        # iteratively merge the most common pairs to create new tokens
         merges = {}
         vocab = {idx: bytes([idx]) for idx in range(256)}
         for i in range(num_merges):
             stats = get_stats(ids)
+
+            # find the pair with the highest count
             pair = max(stats, key=stats.get)
+
+            # mint a new token: assign it the next available id
             idx = 256 + i
+
+            # replace all occurrences of pair in ids with idx
             ids = merge(ids, pair, idx)
             merges[pair] = idx
             vocab[idx] = vocab[pair[0]] + vocab[pair[1]]
@@ -38,10 +48,17 @@ class BpeTokenizer:
         text_bytes = text.encode("utf-8")
         ids = list(text_bytes)
         while len(ids) >= 2:
+
+            # find the pair with the lowest merge index
             stats = get_stats(ids)
+
+            # if there are no more merges available, the key will result in an infinite for every single pair,
+            # and the min will be the first pair in the list, arbitrarily
             pair = min(stats, key=lambda p: self.merges.get(p, float("inf")))
             if pair not in self.merges:
                 break
+
+            # otherwise let's merge the best pair (lowest merge index)
             idx = self.merges[pair]
             ids = merge(ids, pair, idx)
         return ids
